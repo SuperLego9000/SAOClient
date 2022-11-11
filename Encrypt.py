97962801f8910bb4b7828b5dba56b9d8cf62ab23
SuperLego9000 <82184903+SuperLego9000@users.noreply.github.com>
2022-11-11 10:35:27 -0800

    encode
    encode and decode support

---
+++ Encrypt.py
@@ -0,0 +1,21 @@
+import base64
+import json
+import JsonUtils as jsu
+def decode(key):
+    raw=base64.b64decode(key).decode()
+    #raw=str(raw)
+    #print(raw)
+    #decrypted
+    data=json.loads(raw)
+    #print(data)
+    #data={key,name}
+    return data
+def encode(arr):
+    raw=json.dumps(arr)
+    raw=base64.b64encode(bytes(raw,"utf-8")).decode()
+    return raw
+test="WyJ0aGUgam9lcyIsIldBR0hSViJd"
+if __name__=="__main__":
+    x=encode(["joe gang","abcxyz"])
+    y=decode(x)
+    print(x,y)
